768b91c959fa09278953da42bc4ea626214fc68d
Geoffrey Wu <geoffrey.feifei@gmail.com>
2023-01-25 16:58:36 -0500

    move into `backup/` folder

--- backups/download-backup.sh
+++ backups/download-backup.sh
@@ -3,7 +3,7 @@
 # Dump the database to generate a backup
 # mongodump and bsondump can be installed from the MongoDB Database Tools
 
-#.env should be a file with the MONGODB_USERNAME and MONGODB_PASSWORD
+# .env should be a file with the MONGODB_USERNAME and MONGODB_PASSWORD in the same directory as this file
 # defined as bash variables (a standard .env file should do the trick)
 source .env
 mongodump --uri="mongodb+srv://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@qbreader.0i7oej9.mongodb.net/?retryWrites=true&w=majority" -d qbreader
