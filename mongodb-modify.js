5407df563a087168a8041837e47080e8c737d488
Geoffrey Wu <geoffrey.feifei@gmail.com>
2023-02-27 20:38:37 -0500

    add `fixAcceptEither`

--- mongodb-modify.js
+++ mongodb-modify.js
@@ -17,6 +17,7 @@ const bonuses = database.collection('bonuses');
 
 
 const cats = require('./subcat-to-cat.json');
+const { tossupToString, bonusToString } = require('./stringify');
 
 
 function clearReports() {
@@ -79,6 +80,52 @@ function denormalizeSetYears() {
         }
     });
 }
+
+
+const fixAcceptEither = (() => {
+    const removeParentheses = (string) => {
+        return string
+            .replace(/\([^)]*\)/g, '')
+            .replace(/\[[^\]]*\]/g, '');
+    };
+
+    const splitMainAnswer = (string) => {
+        const bracketsSubAnswer = (string.match(/(?<=\[)[^\]]*(?=\])/g) ?? ['']).pop();
+        const parenthesesSubAnswer = (string.match(/(?<=\()[^)]*(?=\))/g) ?? ['']).pop();
+
+        const mainAnswer = removeParentheses(string);
+
+        if (bracketsSubAnswer.length !== 0)
+            return { mainAnswer, subAnswer: bracketsSubAnswer };
+
+        for (const directive of ['or', 'prompt', 'antiprompt', 'anti-prompt', 'accept', 'reject', 'do not accept']) {
+            if (parenthesesSubAnswer.toLowerCase().startsWith(directive))
+                return { mainAnswer, subAnswer: parenthesesSubAnswer };
+        }
+
+        return { mainAnswer, subAnswer: '' };
+    };
+
+    return async function () {
+        let count = 0;
+        tossups.find({ formatted_answer: { $regex: /[a-z][A-Z].*[[(]accept either/ } }).forEach(async tossup => {
+            const { mainAnswer, subAnswer } = splitMainAnswer(tossup?.formatted_answer ?? '');
+            if (mainAnswer.match(/(?<=<u>)[^<]*(?=<\/u>)/g)?.length === 1) {
+                await tossups.updateOne(
+                    { _id: tossup._id },
+                    { $set: {
+                        formatted_answer: tossup.formatted_answer.replace(/(?<=[a-z])(?=[A-Z])/, '</u></b> <b><u>'),
+                        answer: tossup.answer.replace(/(?<=[a-z])(?=[A-Z])/, ' ')
+                    } }
+                );
+
+                count++;
+                if (count % 50 === 0)
+                    console.log(count);
+            }
+        });
+    };
+})();
 
 
 function fixPacketOrders() {
