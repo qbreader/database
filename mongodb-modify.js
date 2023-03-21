aa172e8226a9be73184d5e55b81716aafcbe6566
Geoffrey Wu <geoffrey.feifei@gmail.com>
2023-03-21 18:45:26 -0400

    add `removeTrailingBonusesString`

--- mongodb-modify.js
+++ mongodb-modify.js
@@ -374,6 +374,27 @@ async function renameSet(oldName, newName) {
 }
 
 
+async function removeTrailingBonusesString() {
+    const count = await tossups.countDocuments({ answer: /bonuses/i });
+    console.log(count);
+
+    tossups.find({ answer: /bonuses/i }).forEach(tossup => {
+        tossups.updateOne(
+            { _id: tossup._id },
+            { $set: {
+                answer: tossup.answer.replace(/bonuses.*/i, ''),
+                updatedAt: new Date(),
+            } }
+        );
+
+        if (tossup.formatted_answer) {
+            tossups.updateOne(
+                { _id: tossup._id },
+                { $set: { formatted_answer: tossup.formatted_answer.replace(/(<b><u>)?bonuses.*/i, '') } }
+            );
+        }
+    });
+}
 async function sanitizeLeadin() {
     const regExp = /(?<=each:) {2}.*/i;
     const count = await bonuses.countDocuments({ leadin: { $regex: regExp } });
