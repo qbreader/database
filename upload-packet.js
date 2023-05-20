d69f008d19de1629ba773ee48beeaec4944afc2e
Geoffrey Wu <geoffreywu1000@gmail.com>
2023-05-20 12:14:31 -0500

    remove conditional `dotenv` import

--- upload-packet.js
+++ upload-packet.js
@@ -1,6 +1,4 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
-}
+require('dotenv').config();
 
 const fs = require('fs');
 const { MongoClient, ObjectId } = require('mongodb');
