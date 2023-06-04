df41b75ecdf00084d5fb88f04cd5cf53d7391e4e
Geoffrey Wu <geoffreywu1000@gmail.com>
2023-06-03 22:27:05 -0500

    add ability to dump entire cluster

--- backups/download-backup.sh
+++ backups/download-backup.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-# Dump the database to generate a backup
+# Dump all databases in the cluster to generate a backup
 # mongodump and bsondump can be installed from the MongoDB Database Tools
 
 # .env should be a file with the MONGODB_USERNAME and MONGODB_PASSWORD in the same directory as this file
 # defined as bash variables (a standard .env file should do the trick)
 source .env
-mongodump --uri="mongodb+srv://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@qbreader.0i7oej9.mongodb.net/?retryWrites=true&w=majority" -d qbreader
-mv dump/qbreader $(date +%Y-%m-%d_%H:%M:%S)
-rm -r dump
-cd $(date +%Y-%m-%d_%H:%M:%S)
-bsondump --outFile=tossups.json tossups.bson
-bsondump --outFile=bonuses.json bonuses.bson
-bsondump --outFile=sets.json sets.bson
+mongodump --uri="mongodb+srv://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@qbreader.0i7oej9.mongodb.net/?retryWrites=true&w=majority"
