eae5f581db40d906c80721750e42aad97afb9b8a
Geoffrey Wu <geoffreywu1000@gmail.com>
2023-06-26 19:47:29 -0400

    add deleteBuzzes, deletePacet, manuallyAddPayment

--- geoword/modify.js
+++ geoword/modify.js
@@ -6,20 +6,6 @@ const uri = `mongodb+srv://${process.env.MONGODB_USERNAME || 'geoffreywu42'}:${p
 const client = new MongoClient(uri);
 client.connect().then(async () => {
     console.log('connected to mongodb');
-    // console.log(await tossups.updateMany({ division: 'Division 1.json' }, { $set: { division: 'Division 1' } }));
-    // buzzes.updateMany({}, { $set: { division: 'High School' } });
-    // await buzzes.aggregate([
-    //     { $group: {
-    //         _id: '$user_id',
-    //     } },
-    // ]).forEach(user => {
-    //     divisionChoices.insertOne({
-    //         user_id: user._id,
-    //         division: 'High School',
-    //         packetName: 'beterword-sample',
-    //     });
-    // });
-    // console.log(await buzzes.deleteMany({ user_id: new ObjectId('6462a3908039ff856023d8fa') }));
     client.close();
 });
 
@@ -27,7 +13,46 @@ const geoword = client.db('geoword');
 const buzzes = geoword.collection('buzzes');
 const divisionChoices = geoword.collection('division-choices');
 const packets = geoword.collection('packets');
+const payments = geoword.collection('payments');
 const tossups = geoword.collection('tossups');
+
+const accountInfo = client.db('account-info');
+const users = accountInfo.collection('users');
+
+async function deleteBuzzes(packetName) {
+    await buzzes.deleteMany({ packetName });
+}
+
+async function deleteAdminBuzzes(packetName) {
+    const admins = await users.find({ admin: true }).toArray();
+
+    for (const admin of admins) {
+        console.log(admin.username);
+        console.log(await buzzes.deleteMany({ user_id: admin._id, packetName }));
+    }
+}
+
+async function deletePacket(packetName) {
+    await packets.deleteOne({ name: packetName });
+    await tossups.deleteMany({ packetName });
+    await buzzes.deleteMany({ packetName });
+}
+
+async function manuallyAddPayment(username, packetName) {
+    const user = await users.findOne({ username });
+    if (!user) {
+        return null;
+    }
+
+    const user_id = user._id;
+
+    return await payments.replaceOne(
+        { user_id, packetName },
+        { user_id, packetName, createdAt: new Date(), manual: true },
+        { upsert: true }
+    );
+}
+
 
 async function renamePacket(oldName, newName) {
     await buzzes.updateMany({ packetName: oldName }, { $set: { packetName: newName } });
