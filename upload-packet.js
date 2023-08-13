57c66e9ec4460fcdbf38b8ec285c185fe2a6761a
Geoffrey Wu <geoffreywu1000@gmail.com>
2023-08-13 11:31:34 -0500

    don't delete existing packets and questions

--- upload-packet.js
+++ upload-packet.js
@@ -16,8 +16,11 @@ const packets = database.collection('packets');
 const sets = database.collection('sets');
 const tossups = database.collection('tossups');
 
+const tossupBulk = tossups.initializeUnorderedBulkOp();
+const bonusBulk = bonuses.initializeUnorderedBulkOp();
+
 /**
- * Upload a packet without deleting existing _id references.
+ * Upload a packet without deleting existing `_id` references.
  * Creates new packets _as needed_.
  * @param {string} setName
  * @param {string} packetName
@@ -26,28 +29,29 @@ const tossups = database.collection('tossups');
  */
 async function uploadPacket(setName, packetName, packetNumber, shiftPacketNumbers = true) {
     const data = JSON.parse(fs.readFileSync(`${packetName}.json`));
+    let packetAlreadyExists = false;
 
-    const set_id = await sets.findOne({ name: setName }).then(set => {
-        return set._id;
-    });
+    const set = await sets.findOne({ name: setName });
+    const packet = await packets.findOne({ 'set._id': set._id, number: packetNumber });
 
     let packet_id = new ObjectId();
 
-    if (!shiftPacketNumbers) {
-        packet_id = await packets.findOne({ name: setName, number: packetNumber }).then(packet => packet._id);
-    }
-
     if (shiftPacketNumbers) {
-        console.log(await tossups.updateMany({ set_id: set_id, packetNumber: { $gte: packetNumber } }, { $inc: { packetNumber: 1 } }));
-        console.log(await bonuses.updateMany({ set_id: set_id, packetNumber: { $gte: packetNumber } }, { $inc: { packetNumber: 1 } }));
-        console.log(await packets.updateMany({ 'set._id': set_id, number: { $gte: packetNumber } }, { $inc: { number: 1 } }));
-    } else {
-        console.log(await tossups.deleteMany({ set_id: set_id, packet_id: packet_id }));
-        console.log(await bonuses.deleteMany({ set_id: set_id, packet_id: packet_id }));
-        console.log(await packets.deleteOne({ _id: packet_id }));
+        console.log(await tossups.updateMany({ set_id: set._id, packetNumber: { $gte: packetNumber } }, { $inc: { packetNumber: 1 } }));
+        console.log(await bonuses.updateMany({ set_id: set._id, packetNumber: { $gte: packetNumber } }, { $inc: { packetNumber: 1 } }));
+        console.log(await packets.updateMany({ 'set._id': set._id, number: { $gte: packetNumber } }, { $inc: { number: 1 } }));
+    } else if (packet) {
+        packetAlreadyExists = true;
+        packet_id = packet._id;
     }
 
-    await packets.insertOne({ _id: packet_id, name: packetName, number: packetNumber, set: { _id: set_id, name: setName } });
+    if (packetAlreadyExists) {
+        await packets.updateOne({ _id: packet_id }, { $set: { name: packetName } });
+    } else {
+        await packets.insertOne({ _id: packet_id, name: packetName, number: packetNumber, set: { _id: set._id, name: setName } });
+    }
+
+    const tossupCount = await tossups.countDocuments({ packet_id: packet_id });
 
     data.tossups.forEach(async (tossup, index) => {
         tossup.question = tossup.question
@@ -67,18 +71,39 @@ async function uploadPacket(setName, packetName, packetNumber, shiftPacketNumber
                 .trim();
         }
 
-        tossup._id = new ObjectId();
-        tossup.packet_id = packet_id;
-        tossup.set_id = set_id;
-        tossup.setName = setName;
-        tossup.type = 'tossup';
-        tossup.packetNumber = packetNumber;
-        tossup.packetName = packetName;
-        tossup.questionNumber = tossup.questionNumber || (index + 1);
-        tossup.createdAt = tossup.createdAt || new Date();
-        tossup.updatedAt = tossup.updatedAt || new Date();
-        await tossups.insertOne(tossup);
+        const updateDoc = {
+            $set: {
+                question: tossup.question,
+                answer: tossup.answer,
+                formatted_answer: tossup.formatted_answer,
+                updatedAt: new Date(),
+                category: tossup.category,
+                subcategory: tossup.subcategory,
+                packetName: packetName,
+            },
+            $unset: {
+                reports: '',
+            }
+        };
+
+        if (index < tossupCount && packetAlreadyExists) {
+            tossupBulk.find({ packet_id: packet_id, questionNumber: index + 1 }).updateOne(updateDoc);
+        } else {
+            tossupBulk.insert({
+                packet_id: packet_id,
+                questionNumber: index + 1,
+                set_id: set._id,
+                type: 'tossup',
+                createdAt: new Date(),
+                packetNumber: packetNumber,
+                setName: setName,
+                setYear: set.year,
+                ...updateDoc.$set,
+            });
+        }
     });
+
+    const bonusCount = await bonuses.countDocuments({ packet_id: packet_id });
 
     data.bonuses.forEach(async (bonus, index) => {
         for (let i = 0; i < bonus.parts.length; i++) {
@@ -102,18 +127,43 @@ async function uploadPacket(setName, packetName, packetNumber, shiftPacketNumber
                 .trim();
         }
 
-        bonus._id = new ObjectId();
-        bonus.packet_id = packet_id;
-        bonus.set_id = set_id;
-        bonus.setName = setName;
-        bonus.type = 'bonus';
-        bonus.packetNumber = packetNumber;
-        bonus.packetName = packetName;
-        bonus.questionNumber = bonus.questionNumber || (index + 1);
-        bonus.createdAt = bonus.createdAt || new Date();
-        bonus.updatedAt = bonus.updatedAt || new Date();
-        await bonuses.insertOne(bonus);
+        const updateDoc = {
+            $set: {
+                leadin: bonus.leadin,
+                parts: bonus.parts,
+                answers: bonus.answers,
+                formatted_answers: bonus.formatted_answers,
+                updatedAt: new Date(),
+                category: bonus.category,
+                subcategory: bonus.subcategory,
+                values: bonus.values,
+                difficulties: bonus.difficulties,
+                packetName: packetName,
+            },
+            $unset: {
+                reports: '',
+            }
+        };
+
+        if (index < bonusCount && packetAlreadyExists) {
+            bonusBulk.find({ packet_id: packet_id, questionNumber: index + 1 }).updateOne(updateDoc);
+        } else {
+            bonusBulk.insert({
+                packet_id: packet_id,
+                questionNumber: index + 1,
+                set: set._id,
+                type: 'bonus',
+                createdAt: new Date(),
+                packetNumber: packetNumber,
+                setName: setName,
+                setYear: set.year,
+                ...updateDoc.$set,
+            });
+        }
     });
+
+    console.log(await tossupBulk.execute());
+    console.log(await bonusBulk.execute());
 }
 
 export default uploadPacket;
