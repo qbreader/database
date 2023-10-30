af80f78e414513a8d9d0526807d71daf6e92fc76
Geoffrey Wu <geoffreywu1000@gmail.com>
2023-10-29 23:47:58 -0400

    clean up code into functions

--- geoword/upload-audio.js
+++ geoword/upload-audio.js
@@ -3,51 +3,44 @@ import 'dotenv/config';
 import * as fs from 'fs';
 import { MongoClient, ObjectId } from 'mongodb';
 
-const PACKET_DIRECTORY = 'packets/';
-const EXTENSION = '.mp3';
-
 const uri = `mongodb+srv://${process.env.MONGODB_USERNAME || 'geoffreywu42'}:${process.env.MONGODB_PASSWORD || 'password'}@qbreader.0i7oej9.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri);
-client.connect().then(async () => {
-    console.log('connected to mongodb');
+await client.connect();
 
-    const database = client.db('geoword');
-    const audio = database.collection('audio');
-    const tossups = database.collection('tossups');
+console.log('connected to mongodb');
 
-    for (const packetName of fs.readdirSync(PACKET_DIRECTORY)) {
-        if (packetName === '.DS_Store') {
-            continue;
-        }
+const database = client.db('geoword');
+const audio = database.collection('audio');
+const tossups = database.collection('tossups');
 
-        console.log('Uploading', packetName);
 
-        for (const division of fs.readdirSync(PACKET_DIRECTORY + packetName)) {
-            if (division === '.DS_Store' || division === 'sample.mp3') {
+/**
+ * Upload the audio from a packet.
+ * Requires the questions from the packet to be uploaded first (see `upload-packets.js`).
+ * @param {string} packetName
+ * @param {string} [extension='mp3']
+ * @param {string} [packetDirectory='./']
+ */
+async function uploadAudio(packetName, extension = 'mp3', packetDirectory = './') {
+    const divisions = fs.readdirSync(`${packetDirectory}/${packetName}`).map(division => division.replace('.json', ''));
+
+    for (const division of divisions) {
+        let counter = 0;
+
+        for (const filename of fs.readdirSync(`${packetDirectory}/${packetName}/${division}`)) {
+            if (!filename.endsWith(extension)) {
                 continue;
             }
-
-            if (!['High School', 'Division 1', 'Division 2'].includes(division)) {
-                console.log('wrong division:', division);
-                continue;
-            }
-
-            let counter = 0;
-
-            for (const filename of fs.readdirSync(PACKET_DIRECTORY + packetName + '/' + division)) {
-                if (!filename.endsWith(EXTENSION)) {
-                    continue;
-                }
-
-                const questionNumber = parseInt(filename.slice(0, -EXTENSION.length));
-                const _id = new ObjectId();
-                const file = fs.readFileSync(PACKET_DIRECTORY + packetName + '/' + division + '/' + filename);
-                await audio.insertOne({ _id: _id, audio: file });
-                await tossups.updateOne({ 'packet.name': packetName, division, questionNumber }, { $set: { audio_id: _id } });
-                counter++;
-            }
-
-            console.log(`Uploaded ${division} of ${packetName} (${counter} files)`);
+            const questionNumber = parseInt(filename.slice(0, -extension.length - 1));
+            const _id = new ObjectId();
+            const file = fs.readFileSync(`${packetDirectory}/${packetName}/${division}/${filename}`);
+            await audio.insertOne({ _id: _id, audio: file });
+            await tossups.updateOne({ 'packet.name': packetName, division, questionNumber }, { $set: { audio_id: _id } });
+            counter++;
         }
+
+        console.log(`Uploaded ${division} of ${packetName} (${counter} files)`);
     }
-});
+}
+
+export default uploadAudio;
---
+++ geoword/upload-packet.js
@@ -0,0 +1,74 @@
+import 'dotenv/config';
+
+import * as fs from 'fs';
+import { MongoClient, ObjectId } from 'mongodb';
+
+const uri = `mongodb+srv://${process.env.MONGODB_USERNAME || 'geoffreywu42'}:${process.env.MONGODB_PASSWORD || 'password'}@qbreader.0i7oej9.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri);
+await client.connect();
+
+console.log('connected to mongodb');
+
+const database = client.db('geoword');
+const packets = database.collection('packets');
+const tossupCollection = database.collection('tossups');
+
+/**
+ *
+ * @param {string} packetName
+ * @param {ObjectId} packet_id
+ * @param {string} division
+ * @param {string} [packetDirectory='./']
+ */
+async function uploadDivision(packetName, packet_id, division, packetDirectory = './') {
+    const data = JSON.parse(fs.readFileSync(`${packetDirectory}/${packetName}/${division}.json`));
+    const { tossups } = data;
+
+    await tossupCollection.deleteMany({ packetName, division });
+
+    for (let index = 0; index < tossups.length; index++) {
+        const tossup = tossups[index];
+
+        tossup.question = tossup.question.replace(/\t/g, ' ').replace(/ {2,}/g, ' ').trim();
+        tossup.answer = tossup.answer.replace(/\t/g, ' ').replace(/ {2,}/g, ' ').trim();
+
+        if (tossup.formatted_answer) {
+            tossup.formatted_answer = tossup.formatted_answer.replace(/\t/g, ' ').replace(/ {2,}/g, ' ').trim();
+        }
+
+        tossup.division = tossup.division || division;
+        tossup.packet = {
+            name: packetName,
+            _id: packet_id,
+        };
+        tossup.questionNumber = tossup.questionNumber || (index + 1);
+    }
+
+    console.log(await tossupCollection.insertMany(tossups));
+}
+
+/**
+ * Uploads a single packet, deleting any questions under the same packetName that were previously uploaded.
+ * The packet should be in a folder with the same name as `packetName` in the `packetDirectory` directory.
+ * Each division should be a separate JSON file in the folder corresponding to the questions for that division.
+ * @param {string} packetName
+ * @param {number} [costInCents=250]
+ * @param {*} [packetDirectory='./']
+ */
+async function uploadPacket(packetName, costInCents = 250, packetDirectory = './') {
+    const divisions = fs.readdirSync(`${packetDirectory}/${packetName}`).map(division => division.replace('.json', ''));
+    console.log('uploading packet', packetName, 'with divisions', divisions);
+    const packet_id = new ObjectId();
+
+    for (const division of divisions) {
+        await uploadDivision(packetName, packet_id, division, packetDirectory);
+    }
+
+    console.log(await packets.replaceOne(
+        { name: packetName, divisions },
+        { _id: packet_id, name: packetName, divisions, test: true, active: false, costInCents },
+        { upsert: true },
+    ));
+}
+
+export default uploadPacket;
