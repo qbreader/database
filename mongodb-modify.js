d178ffeeeb1c05a3dcbeda3e1a6291a3f13c6dd9
Geoffrey Wu <geoffreywu1000@gmail.com>
2023-11-07 21:35:16 -0500

    update `deleteSet` to delete appropriate packets

--- mongodb-modify.js
+++ mongodb-modify.js
@@ -60,6 +60,7 @@ async function deleteSet(setName) {
 
     console.log(await tossups.deleteMany({ 'set._id': _id }));
     console.log(await bonuses.deleteMany({ 'set._id': _id }));
+    console.log(await packets.deleteMany({ 'set._id': _id }));
     console.log(await tossupData.deleteMany({ set_id: _id }));
     console.log(await bonusData.deleteMany({ set_id: _id }));
 }
