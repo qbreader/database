f5ff317bea7ff13e44f93f35e3eb689e70acf7ce
Geoffrey Wu <geoffreywu1000@gmail.com>
2024-01-06 22:23:10 -0600

    create `updateAlternateSubcategory`

--- utilities/question-updates/index.js
+++ utilities/question-updates/index.js
@@ -1,11 +1,13 @@
 import clearReports from './clear-reports.js';
 import standardizeSubcategories from './standardize-subcategories.js';
+import updateAlternateSubcategory from './update-alternate-subcategory.js';
 import updateManySubcategories from './update-many-subcategories.js';
 import updateSubcategory from './update-subcategory.js';
 
 export {
     clearReports,
     standardizeSubcategories,
+    updateAlternateSubcategory,
     updateManySubcategories,
     updateSubcategory,
 };
