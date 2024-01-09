d8b7728ea0a6730e9201073482d874ab5ac6114f
Geoffrey Wu <geoffreywu1000@gmail.com>
2024-01-09 17:24:22 -0600

    validateDeprecatedFields

--- utilities/validation/index.js
+++ utilities/validation/index.js
@@ -1,5 +1,6 @@
 import alternateSubcategoryValidation from './alternate-subcategory.js';
 import categoryValidation from './category.js';
+import deprecatedFieldsValidation from './deprecated-fields.js';
 import packetValidation from './packets.js';
 import setValidation from './sets.js';
 import subcategoryValidation from './subcategory.js';
@@ -7,6 +8,7 @@ import subcategoryValidation from './subcategory.js';
 export {
     alternateSubcategoryValidation,
     categoryValidation,
+    deprecatedFieldsValidation,
     packetValidation,
     setValidation,
     subcategoryValidation,
