44773af6e638a2c20a0cb712b42f439c19ff43bf
Geoffrey Wu <geoffreywu1000@gmail.com>
2024-01-09 17:28:27 -0600

    validate everything

---
+++ utilities/validation/everything.js
@@ -0,0 +1,22 @@
+import alternateSubcategoryValidation from './alternate-subcategory.js';
+import categoryValidation from './category.js';
+import deprecatedFieldsValidation from './deprecated-fields.js';
+import packetValidation from './packets.js';
+import setValidation from './sets.js';
+import subcategoryValidation from './subcategory.js';
+
+
+export default async function validateEverything(verbose = true) {
+    let total = 0;
+
+    total += await alternateSubcategoryValidation(verbose);
+    total += await categoryValidation(verbose);
+    total += await subcategoryValidation(verbose);
+
+    total += await packetValidation(verbose);
+    total += await setValidation(verbose);
+
+    total += await deprecatedFieldsValidation(verbose);
+
+    return total;
+}
--- utilities/validation/index.js
+++ utilities/validation/index.js
@@ -1,6 +1,7 @@
 import alternateSubcategoryValidation from './alternate-subcategory.js';
 import categoryValidation from './category.js';
 import deprecatedFieldsValidation from './deprecated-fields.js';
+import everythingValidation from './everything.js';
 import packetValidation from './packets.js';
 import setValidation from './sets.js';
 import subcategoryValidation from './subcategory.js';
@@ -9,6 +10,7 @@ export {
     alternateSubcategoryValidation,
     categoryValidation,
     deprecatedFieldsValidation,
+    everythingValidation,
     packetValidation,
     setValidation,
     subcategoryValidation,
