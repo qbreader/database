b5bb92ae1e31cb39b93656fcac039403989fb257
Geoffrey Wu <geoffreywu1000@gmail.com>
2024-05-06 23:35:09 -0400

    add fix-leadin-bonus.js

--- utilities/batch-fixes/index.js
+++ utilities/batch-fixes/index.js
@@ -1,4 +1,5 @@
 import fixAcceptEither from './fix-accept-either.js';
+import fixLeadinBonus from './fix-leadin-bonus.js';
 import fixLeadingColons from './fix-leading-colons.js';
 import fixSpaces from './fix-spaces.js';
 import removeTrailingBonusesString from './remove-trailing-bonuses-string.js';
@@ -7,6 +8,7 @@ import sanitizeQuestions from './sanitize-questions.js';
 
 export {
     fixAcceptEither,
+    fixLeadinBonus,
     fixLeadingColons,
     fixSpaces,
     removeTrailingBonusesString,
