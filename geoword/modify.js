4067fc2214ebf80ae3001c080194872401b33bce
Geoffrey Wu <geoffreywu1000@gmail.com>
2025-05-13 00:32:10 -0400

    update database uri in geoword/

--- geoword/modify.js
+++ geoword/modify.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 
 import { MongoClient, ObjectId } from 'mongodb';
 
-const uri = `mongodb+srv://${process.env.MONGODB_USERNAME || 'geoffreywu42'}:${process.env.MONGODB_PASSWORD || 'password'}@qbreader.0i7oej9.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.MONGODB_USERNAME || 'geoffreywu42'}:${process.env.MONGODB_PASSWORD || 'password'}@qbreader2.z35tynb.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri);
 await client.connect();
 console.log('connected to mongodb');
