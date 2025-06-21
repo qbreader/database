19537a5c41171e6d839e2a36e977f54ae2f80d33
Geoffrey Wu <geoffreywu1000@gmail.com>
2025-06-21 12:02:49 -0400

    delete unnecessary question-lists functions

--- utilities/question-lists/index.js
+++ utilities/question-lists/index.js
@@ -1,13 +1,7 @@
-import listAlternateSubcategories from './list-alternate-subcategories.js';
-import listQuestionsWithoutSubcategory from './list-questions-without-subcategory.js';
-import listSetsWithAnswerFormatting from './list-sets-with-answer-formatting.js';
-import listSetsWithoutField from './list-sets-without-field.js';
 import printMostReadBonuses from './print-most-read-bonuses.js';
 import printMostReadTossups from './print-most-read-tossups.js';
 
 export {
-  listSetsWithAnswerFormatting,
-  listSetsWithoutField,
   printMostReadBonuses,
   printMostReadTossups
 };
