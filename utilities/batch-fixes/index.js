02851b2329baa9730178a3027faf4e14cf1c430d
Geoffrey Wu <geoffreywu1000@gmail.com>
2025-06-23 12:22:03 -0400

    delete unnecessary utilities

--- utilities/batch-fixes/index.js
+++ utilities/batch-fixes/index.js
@@ -3,15 +3,11 @@ import fixLeadinBonus from './fix-leadin-bonus.js';
 import fixLeadingColons from './fix-leading-colons.js';
 import fixSpaces from './fix-spaces.js';
 import removeTrailingBonusesString from './remove-trailing-bonuses-string.js';
-import sanitizeLeadin from './sanitize-leadin.js';
-import sanitizeQuestions from './sanitize-questions.js';
 
 export {
   fixAcceptEither,
   fixLeadinBonus,
   fixLeadingColons,
   fixSpaces,
-  removeTrailingBonusesString,
-  sanitizeLeadin,
-  sanitizeQuestions
+  removeTrailingBonusesString
 };
--- utilities/question-updates/index.js
+++ utilities/question-updates/index.js
@@ -1,17 +1,9 @@
-import addUnformattedFields from './add-unformatted-fields.js';
-import boldPowermarks from './bold-powermarks.js';
 import clearReports from './clear-reports.js';
-import deleteDeprecatedFields from './delete-deprecated-fields.js';
-import standardizeSubcategories from './standardize-subcategories.js';
 import updateManySubcategories from './update-many-subcategories.js';
 import updateSubcategory from './update-subcategory.js';
 
 export {
-  addUnformattedFields,
-  boldPowermarks,
   clearReports,
-  deleteDeprecatedFields,
-  standardizeSubcategories,
   updateManySubcategories,
   updateSubcategory
 };
