d8d6a931b068224ef5c77a342753357991db6833
Geoffrey Wu <geoffreywu1000@gmail.com>
2025-06-30 21:18:55 -0500

    delete more unnecessary utilities

--- utilities/batch-fixes/index.js
+++ utilities/batch-fixes/index.js
@@ -1,11 +1,7 @@
 import fixAcceptEither from './fix-accept-either.js';
-import fixLeadingColons from './fix-leading-colons.js';
 import fixSpaces from './fix-spaces.js';
-import removeTrailingBonusesString from './remove-trailing-bonuses-string.js';
 
 export {
   fixAcceptEither,
-  fixLeadingColons,
-  fixSpaces,
-  removeTrailingBonusesString
+  fixSpaces
 };
--- utilities/question-lists/index.js
+++
@@ -1,7 +0,0 @@
-import printMostReadBonuses from './print-most-read-bonuses.js';
-import printMostReadTossups from './print-most-read-tossups.js';
-
-export {
-  printMostReadBonuses,
-  printMostReadTossups
-};
