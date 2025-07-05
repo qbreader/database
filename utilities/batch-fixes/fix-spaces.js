397ae325155ed12b522ea6399dd27b6c1ed0b9fc
Geoffrey Wu <geoffreywu1000@gmail.com>
2025-07-05 11:15:33 -0500

    Update fix-spaces.js

--- utilities/batch-fixes/fix-spaces.js
+++ utilities/batch-fixes/fix-spaces.js
@@ -1,84 +1,86 @@
 import { tossups, bonuses } from '../collections.js';
+import sanitizeString from '../../core/sanitize-string.js';
 
-export default async function fixSpaces () {
-  const fields = ['question', 'question_sanitized', 'answer', 'answer_sanitized', 'leadin', 'leadin_sanitized'];
-  const arrayFields = ['parts', 'parts_sanitized', 'answers', 'answers_sanitized'];
-
-  for (const field of fields) {
-    console.log(field, await tossups.countDocuments({ [field]: /\t|^ | $| {2,}/ }));
-    console.log(field, await bonuses.countDocuments({ [field]: /\t|^ | $| {2,}/ }));
+/**
+ *
+ * @param {'bonus' | 'tossup'} type
+ * @returns
+ */
+function typeToCollection (type) {
+  if (type === 'tossup') {
+    return tossups;
+  } else if (type === 'bonus') {
+    return bonuses;
   }
+  throw new Error(`Unknown type: ${type}`);
+}
 
-  for (const field of arrayFields) {
-    console.log(field, await tossups.countDocuments({ [field]: /\t|^ | $| {2,}/ }));
-    console.log(field, await bonuses.countDocuments({ [field]: /\t|^ | $| {2,}/ }));
-  }
+const spaceRegex = /\t|^ | $| {2,}/g;
+
+/**
+ * Fixes extra spaces in specified fields of 'tossup' and 'bonus' documents in the database.
+ *
+ * @param {Object} [options={}] - Options for the function.
+ * @param {boolean} [options.performUpdates=false] - If true, updates the documents in the database; otherwise, only logs the counts.
+ * @returns {Promise<void>} Resolves when the operation is complete.
+ */
+export default async function fixSpaces ({ performUpdates = false } = {}) {
+  const fields = {
+    tossup: ['question', 'answer'],
+    bonus: ['leadin']
+  };
+  const arrayFields = {
+    tossup: [],
+    bonus: ['parts', 'answers']
+  };
 
   let counter = 0;
 
-  for (const field of fields) {
-    console.log('updating', field);
-    tossups.find({ [field]: /\t|^ | $| {2,}/ }).forEach(question => {
-      question[field] = question[field]
-        .replace(/\t/g, ' ')
-        .replace(/ {2,}/g, ' ')
-        .trim();
+  for (const type in fields) {
+    for (const field of fields[type]) {
+      const count = await typeToCollection(type).countDocuments({ [field]: spaceRegex });
+      console.log(field, count);
+      console.log(`${count > 0 ? 'updating' : 'skipping'} ${type}.${field} and ${type}.${field}_sanitized`);
+      if (count === 0) { continue; }
+      if (!performUpdates) { continue; }
 
-      tossups.updateOne({ _id: question._id }, { $set: { [field]: question[field] } });
+      for (const question of await typeToCollection(type).find({ [field]: spaceRegex }).toArray()) {
+        question[field] = question[field].replace(spaceRegex, ' ').trim();
 
-      counter++;
-      if (counter % 100 === 0) {
-        console.log(counter);
+        await typeToCollection(type).updateOne(
+          { _id: question._id },
+          { $set: { [field]: question[field], [`${field}_sanitized`]: sanitizeString(question[field]) } }
+        );
+
+        counter++;
+        if (counter % 100 === 0) {
+          console.log(counter);
+        }
       }
-    });
-
-    bonuses.find({ [field]: /\t|^ | $| {2,}/ }).forEach(question => {
-      question[field] = question[field]
-        .replace(/\t/g, ' ')
-        .replace(/ {2,}/g, ' ')
-        .trim();
-
-      bonuses.updateOne({ _id: question._id }, { $set: { [field]: question[field] } });
-
-      counter++;
-      if (counter % 100 === 0) {
-        console.log(counter);
-      }
-    });
+    }
   }
 
-  for (const field of arrayFields) {
-    console.log('updating', field);
-    tossups.find({ [field]: /\t|^ | $| {2,}/ }).forEach(question => {
-      for (let i = 0; i < question[field].length; i++) {
-        question[field][i] = question[field][i]
-          .replace(/\t/g, ' ')
-          .replace(/ {2,}/g, ' ')
-          .trim();
+  for (const type in fields) {
+    for (const field of arrayFields[type]) {
+      const count = await typeToCollection(type).countDocuments({ [field]: spaceRegex });
+      console.log(field, count);
+      console.log(`${count > 0 ? 'updating' : 'skipping'} ${type}.${field}[] and ${type}.${field}_sanitized[]`);
+      if (count === 0) { continue; }
+      if (!performUpdates) { continue; }
+
+      for (const question of await typeToCollection(type).find({ [field]: spaceRegex }).toArray()) {
+        question[field] = question[field].map(part => part.replace(spaceRegex, ' ').trim());
+
+        await typeToCollection(type).updateOne(
+          { _id: question._id },
+          { $set: { [field]: question[field], [`${field}_sanitized`]: question[field].map(part => sanitizeString(part)) } }
+        );
+
+        counter++;
+        if (counter % 100 === 0) {
+          console.log(counter);
+        }
       }
-
-      tossups.updateOne({ _id: question._id }, { $set: { [field]: question[field] } });
-
-      counter++;
-      if (counter % 100 === 0) {
-        console.log(counter);
-      }
-    });
-
-    bonuses.find({ [field]: /\t|^ | $| {2,}/ }).forEach(question => {
-      for (let i = 0; i < question[field].length; i++) {
-        question[field][i] = question[field][i]
-          .replace(/\t/g, ' ')
-          .replace(/ {2,}/g, ' ')
-          .trim();
-      }
-
-      bonuses.updateOne({ _id: question._id }, { $set: { [field]: question[field] } });
-
-      counter++;
-      if (counter % 100 === 0) {
-        console.log(counter);
-      }
-    });
+    }
   }
 }
