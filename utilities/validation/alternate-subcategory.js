14b6e6a53356640ecf0f8033efee11656dc301fa
Geoffrey Wu <geoffreywu1000@gmail.com>
2025-12-10 00:27:18 -0500

    validate alternate subcategories and return total

--- utilities/validation/alternate-subcategory.js
+++ utilities/validation/alternate-subcategory.js
@@ -1,4 +1,5 @@
 import { bonuses, tossups } from '../collections.js';
+import { CATEGORY_TO_ALTERNATE_SUBCATEGORY, SUBCATEGORY_TO_SUBSUBCATEGORY } from '../../categories.js';
 
 const categories = [
   'Literature'
@@ -40,37 +41,61 @@ const alternateSubcategories = [
 ];
 
 export default async function alternateSubcategoryValidation () {
-  let valid = true;
+  let total = 0;
 
   for (const tossup of await tossups.find({ category: { $in: categories }, alternate_subcategory: null }).toArray()) {
     console.log(`Tossup ${tossup._id} has ${tossup.category} category and no alternate subcategory.`);
-    valid = false;
+    total++;
   }
 
   for (const bonus of await bonuses.find({ category: { $in: categories }, alternate_subcategory: null }).toArray()) {
     console.log(`Bonus ${bonus._id} has ${bonus.category} category and no alternate subcategory.`);
-    valid = false;
+    total++;
   }
 
   for (const tossup of await tossups.find({ subcategory: { $in: subcategories }, alternate_subcategory: null }).toArray()) {
     console.log(`Tossup ${tossup._id} has ${tossup.category} / ${tossup.subcategory} subcategory and no alternate subcategory.`);
-    valid = false;
+    total++;
   }
 
   for (const bonus of await bonuses.find({ subcategory: { $in: subcategories }, alternate_subcategory: null }).toArray()) {
     console.log(`Bonus ${bonus._id} has ${bonus.category} / ${bonus.subcategory} subcategory and no alternate subcategory.`);
-    valid = false;
+    total++;
   }
 
   for (const tossup of await tossups.find({ alternate_subcategory: { $not: { $in: alternateSubcategories } } }).toArray()) {
     console.log(`Tossup ${tossup._id} has alternate subcategory ${tossup.alternate_subcategory}, which is not in the list of valid alternate subcategories.`);
-    valid = false;
+    total++;
   }
 
   for (const bonus of await bonuses.find({ alternate_subcategory: { $not: { $in: alternateSubcategories } } }).toArray()) {
     console.log(`Bonus ${bonus._id} has alternate subcategory ${bonus.alternate_subcategory}, which is not in the list of valid alternate subcategories.`);
-    valid = false;
+    total++;
   }
 
-  return valid;
+  for (const [category, validAlternateSubcategories] of Object.entries(CATEGORY_TO_ALTERNATE_SUBCATEGORY)) {
+    for (const tossup of await tossups.find({ category, alternate_subcategory: { $not: { $in: validAlternateSubcategories } } }).toArray()) {
+      console.log(`Tossup ${tossup._id} has category ${category} but alternate subcategory ${tossup.alternate_subcategory}, which is not valid for that category.`);
+      total++;
+    }
+
+    for (const bonus of await bonuses.find({ category, alternate_subcategory: { $not: { $in: validAlternateSubcategories } } }).toArray()) {
+      console.log(`Bonus ${bonus._id} has category ${category} but alternate subcategory ${bonus.alternate_subcategory}, which is not valid for that category.`);
+      total++;
+    }
+  }
+
+  for (const [subcategory, validSubsubcategories] of Object.entries(SUBCATEGORY_TO_SUBSUBCATEGORY)) {
+    for (const tossup of await tossups.find({ subcategory, alternate_subcategory: { $not: { $in: validSubsubcategories } } }).toArray()) {
+      console.log(`Tossup ${tossup._id} has subcategory ${subcategory} but alternate subcategory ${tossup.alternate_subcategory}, which is not valid for that subcategory.`);
+      total++;
+    }
+
+    for (const bonus of await bonuses.find({ subcategory, alternate_subcategory: { $not: { $in: validSubsubcategories } } }).toArray()) {
+      console.log(`Bonus ${bonus._id} has subcategory ${subcategory} but alternate subcategory ${bonus.alternate_subcategory}, which is not valid for that subcategory.`);
+      total++;
+    }
+  }
+
+  return total;
 }
