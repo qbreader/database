0f1433d9422c980a86098b6dddbedd132cd552a0
Geoffrey Wu <geoffreywu1000@gmail.com>
2026-04-12 15:16:32 -0400

    remove copied over utilities

--- utilities/question-updates/index.js
+++
@@ -1,9 +0,0 @@
-import clearReports from './clear-reports.js';
-import updateManySubcategories from './update-many-subcategories.js';
-import updateSubcategory from './update-subcategory.js';
-
-export {
-  clearReports,
-  updateManySubcategories,
-  updateSubcategory
-};
